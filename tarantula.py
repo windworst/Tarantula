a891500cb897b39821c4c71260a5bf5f08517434
XiaoBaWang <windworst@live.com>
2014-01-10 01:58:41 +0800

    0.1+

--- tarantula.py
+++ tarantula.py
@@ -56,39 +56,52 @@ class runner:
 class bfser:
 	bfs_set = 0
 	bfs_list = 0
-	list_iter = 0
+	list_index = 0
 	set_lock = 0
 	instance = 0
+	incheck = 0
 
 	def __init__(self,instance,search_start):
 		self.instance = instance
 		self.bfs_set = set((search_start,))
 		self.bfs_list = [search_start]
-		self.list_iter = iter(self.bfs_list)
+		self.list_index = 0
+		self.in_bfs = 0
 		self.set_lock = threading.Lock()
 
 	def __call__(self):
-		try:
-			self.set_lock.acquire()
-			search_item = self.list_iter.next()
-		except:
-			return False
-		finally:
-			self.set_lock.release()
-		list = instance(search_item)
-
 		self.set_lock.acquire()
-		for item in list
-			if item not in self.bfs_set:
-				self.bfs_list.append(item)
-				self.bfs_set.add(item)
+
+		if self.list_index == len(self.bfs_list):
+			self.set_lock.release()
+			return self.in_bfs
+
+		search_item = self.bfs_list[self.list_index]
+		self.list_index += 1
+
+		self.in_bfs += 1
+
+		self.set_lock.release()
+		
+		list = self.instance(search_item)
+		
+		self.set_lock.acquire()
+		self.in_bfs -= 1
+
+		if list:
+			for item in list:
+				if item not in self.bfs_set:
+					self.bfs_list.append(item)
+					self.bfs_set.add(item)		
 		self.set_lock.release()
 		return True
 
 
 # # # # # # urlcrawler # # # # # # # # # # 
 
-import urllib
+import urllib2
+import re
+import sys
 
 class urlcrawler:
 
@@ -101,23 +114,127 @@ class urlcrawler:
 		self.result = []
 
 	def __call__(self,url):
-		if not url_filter(url):
+		real_url =  self.usable_url(url)
+		if not real_url:
 			return False
-		page = get_page(url)
-		if not page_filter(page):
+		page = self.get_page(real_url)
+		if not page:
 			return False
-		self.collector(url,page)
-		return geturlsfrompage(page)
+		if not self.page_filter(real_url,page):
+			return False
+		self.collector(real_url,page)
+		return self.geturlsfrompage(page)
 
 	def get_page(self,url):
-		return urllib.urlopen(url).read()
+		page = 0
+		try:
+			headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'} 
+			req = urllib2.Request(url,headers = headers)
+			page =  urllib2.urlopen(url).read()
+		except:
+			return False
+		type = sys.getfilesystemencoding()  
+		page = page.decode("gbk","ignore")
+		return page
 
-	def url_filter(self,url):
-		return True
+	def usable_url(self,url):
+		if self.url in url:
+			return url
+		if "http://" not in url:
+			return self.url + url
+		return False
 
 	def page_filter(self,url,page):
 		return True
 
 	def geturlsfrompage(self,page):
-		return []
+		url_list = []
+		url_head = "<a href="
+		list = page.split('\n')
+		p = re.compile("<a\s+href=\\\"\S[^\\\"]+\\\"")
+		for line in list:
+			m = p.search(line)
+			if m:
+				url = m.string[m.start()+len(url_head)+1:m.end()-1]
+				url_list.append(url)
+		return url_list
 
+
+# # # # # # collector # # # # # # # # # # 
+
+def remove_brackets(line):
+	host = ''
+	flag = False
+	for c in line:
+		if not flag:
+			if c == '<':
+				flag = True
+			else:
+				host += c
+		else:
+			if c == '>':
+				flag = False
+				host += ' '
+	return host
+
+import string
+
+def gethostfrompage(page):
+	list = page.split('\n')
+	hostlist = []
+
+	ip_str = 0
+	reip = re.compile('\d+\.\d+\.\d+\.\d+')
+	report = re.compile('\d+')
+	for line in list:
+		mip = 0
+		mport = 0
+		flag = False
+		host = remove_brackets(line)
+
+		if not ip_str:
+			mip = reip.search(host)
+			if mip:
+				ip_str = mip.string[mip.start():mip.end()]
+				mport = report.search(host[mip.end():len(host)])
+		else:
+			mport = report.search(host)
+		if mport:
+			port = string.atoi(mport.string[mport.start():mport.end()])
+			addr = (ip_str,port)
+			hostlist.append(addr)
+			ip_str = 0
+	return hostlist
+
+class simple_collector:
+	result = []
+	mutex = 0
+	def __init__(self):
+		self.result = []
+		self.mutex = threading.Lock()
+
+	def __call__(self,url,page):
+		self.mutex.acquire()
+		print url
+		self.result.append(url+'\n')
+		self.mutex.release()
+
+'''
+"whois".decode("ascii","ignore")
+'''
+import socket
+socket.setdefaulttimeout(5) 
+
+s = simple_collector()
+u = urlcrawler("http://www.cz88.net",s)
+b = bfser(u,"http://www.cz88.net/proxy/index.aspx")
+
+r = runner(b)
+
+open_threads(r,100)
+
+s.result.sort()
+f = open("save.txt","w")
+#for ip,port in s.result:
+#	f.write('%s:%d\n'%(ip,port))
+f.writelines(s.result)
